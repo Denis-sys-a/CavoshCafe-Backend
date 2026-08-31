939b342831746edcaf3bb9d1297f87fda355cc46
Giovanni Denis <1603456@senati.pe>
2026-08-30 22:40:42 -0500

    feat(catalogo): aplicar anotacion JsonProperty para renombrar la salida de los campos en el JSON de respuesta de productos

--- src/main/java/com/cavosh/api_cafe/dto/ProductResponseDTO.java
+++ src/main/java/com/cavosh/api_cafe/dto/ProductResponseDTO.java
@@ -2,6 +2,7 @@ package com.cavosh.api_cafe.dto;
 
 import com.cavosh.api_cafe.entity.ProductCategory;
 import com.cavosh.api_cafe.entity.ProductType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import java.math.BigDecimal;
 
@@ -11,11 +12,24 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Builder
 public class ProductResponseDTO {
+
     private Long id;
+
+    @JsonProperty("producto")
     private String name;
+
+    @JsonProperty("descripcion")
     private String description;
+
+    @JsonProperty("precioBase")
     private BigDecimal basePrice;
+
+    @JsonProperty("imagenUrl")
     private String imageUrl;
+
+    @JsonProperty("categoria")
     private ProductCategory category;
+
+    @JsonProperty("tipoProducto")
     private ProductType tipoProducto;
 }
